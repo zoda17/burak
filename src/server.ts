3b3ac4a113b72cafe347d018923a2585725141b1
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-02 22:56:18 +0900

    BRR: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+// console.log("EXECUTED");
+
+// import moment from "moment";
+
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime);
+
+// const person: string = "Zoda";
+// const count: number = 100;
