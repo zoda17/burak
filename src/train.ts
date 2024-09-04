ede7755698c2de639b85e5b28914ef259561a0a7
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-05 01:05:00 +0900

    feat: create train area

---
+++ src/train.ts
@@ -0,0 +1,18 @@
+// TASK G 
+
+//Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+//MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+// function getHighestIndex(arr: number[]): number {
+//     if (arr.length === 0) return -1; // Return -1 if array is empty
+    
+//     let highestIndex = 0;
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > arr[highestIndex]) {
+//             highestIndex = i;
+//         }
+//     }
+//     return highestIndex;
+// }
+// console.log(getHighestIndex([5, 21, 12, 21, 8]));
+
+
