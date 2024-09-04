45fb60d851a5c2b8b823ab62077c3244f82b1069
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-05 01:57:18 +0900

    feat: Express Integrations

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+
+
+/** 1- ENTRANCE */
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+
+
+/** 2- SESSIONS */
+
+/** 3- VIEWS */
+
+app.set('vies', path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/** 4- ROUTERS */
+
+export default app; // Module.exports =
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import app from "./app";
 
 mongoose.connect(process.env.MONGO_URL as string, {})
 .then((data) => {
     console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function() {
+        console.log(`The server is running successfullymon port: ${PORT}`);
+        
+    })
 })
 .catch(err => console.log("ERROR on connection MongoDB", err));
 
