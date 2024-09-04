a5364d96d522f8f3a6ce2f3d169829f09ee18f98
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-05 01:18:24 +0900

    feat: Connect MongoDB via Mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import mongoose from 'mongoose';
 
+mongoose.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+})
+.catch(err => console.log("ERROR on connection MongoDB", err));
 
 
 
