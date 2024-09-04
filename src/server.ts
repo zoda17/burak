651e3326b675c719b1bf2d566c2e24142f46e698
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-05 00:43:11 +0900

    feat: integration of enviromental variables

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,8 @@
-// console.log("EXECUTED");
+import dotenv from 'dotenv';
+dotenv.config();
 
-// import moment from "moment";
 
-// const currentTime = moment().format("YYYY MM DD");
-// console.log(currentTime);
 
-// const person: string = "Zoda";
-// const count: number = 100;
-
-// Artichectual pattern: MVC, Dependency Injection, MVP, Viper, Three- 
-
-// MVC = MODEL VIEW CONTROLLER
-
-// Design Pattern: Middleware, Decotar
 
 
 
