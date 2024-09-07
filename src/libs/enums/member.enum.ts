c24dda601253a868d9a36c3ca59ece4ffc88f306
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-07 12:48:09 +0900

    feat: Develop  Member Schema Model

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,11 @@
+export enum MemberType {
+    USER = "USER",
+    RESTAURANT = "RESTAURANT",
+}
+
+export enum MemberStatus {
+    ACTIVE = "ACTIBE",
+    BLOCK = "BLOCK",
+    DELETE = "DELETE",
+}
+
---
+++ src/schema/Member.model.ts
@@ -0,0 +1,63 @@
+import mongoose, { Schema } from "mongoose";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
+
+// Schema first & code F
+
+const memberSchema = new Schema({
+memberType: {
+    type: String,
+    enum: MemberType,
+    default: MemberType.USER
+},
+
+memberStatus: {
+    type: String,
+    enum: MemberStatus,
+    default: MemberStatus.ACTIVE
+
+},
+
+memberNick: {
+    type: String,
+    index: {unique: true, sparse: true},
+    required: true,
+},
+
+memberPhone: {
+    type: String,
+    index: { unique: true, sparse: true },
+    required: true
+},
+
+memberPassword: {
+    type: String,
+    select: false,
+    required: true
+},
+
+memberAddress: {
+    type: String,
+    
+},
+
+memberDesc: {
+    type: String,
+    
+},
+
+memberImage: {
+    type: String,
+    
+},
+
+memberPoints: {
+    type: Number,
+    default: 0,
+
+    
+},
+},
+{ timestamps: true } // updatedAt, createAt
+);
+
+export default mongoose.model("Member", memberSchema);
