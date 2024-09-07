4888fbb5175304fff2c45cda6972bba7e763d937
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-07 11:57:30 +0900

    feat: Member service Model Integration

--- src/train.ts
+++ src/train.ts
@@ -21,17 +21,29 @@
 //shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
 //MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
-function getPositive(arr: number[]): string {
-    let positives = arr.filter(num => num > 0);
+// function getPositive(arr: number[]): string {
+//     let positives = arr.filter(num => num > 0);
     
-    let positiveStrings = positives.map(num => num.toString());
+//     let positiveStrings = positives.map(num => num.toString());
     
-    let result = positiveStrings.join('');
+//     let result = positiveStrings.join('');
     
-    return result;
-  }
+//     return result;
+//   }
   
-  console.log(getPositive([5, -6, 78, -8, 99])); 
-  console.log(getPositive([1, -4, 2])); 
+//   console.log(getPositive([5, -6, 78, -8, 99])); 
+//   console.log(getPositive([1, -4, 2])); 
   
   
+//H2-TASK: 
+
+//Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+//MASALAN: getDigits("m14i1t") return qiladi "141"
+
+function getDigits(input: string): string {
+ 
+  return input.split('').filter(char => /\d/.test(char)).join('');
+}
+
+console.log(getDigits("Zo22d11a")); 
+
