c599e1261b868bf9e3f072061ef1bbb69b0fe3a5
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-12 10:53:21 +0900

    feat: TASK I

--- src/train.ts
+++ src/train.ts
@@ -65,3 +65,26 @@ css - Snake                          == button_style
 -- Error handling
 
 */
+
+//Shunday function tuzing, u parametrdagi array ichida eng ko'p
+//takrorlangan raqamni topib qaytarsin.
+
+//MASALAN: majorityElement([1, 2, 3, 4, 5, 4, 3, 4]); return 4
+
+//Yuqoridag misolda argument sifatida kiritilayotgan array tarkibida 4 soni ko'p takrorlanganligi uchun 4'ni return qilmoqda.
+
+function majorityElement(arr: number[]): number | null {
+    let maxCount = 0;
+    let majorityElem: number | null = null;
+
+    arr.forEach(num => {
+        const count = arr.filter(x => x === num).length;
+        if (count > maxCount) {
+            maxCount = count;
+            majorityElem = num;
+        }
+    });
+
+    return majorityElem;
+}
+console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4]));
