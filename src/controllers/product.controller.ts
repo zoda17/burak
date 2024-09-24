f9431be4d0583fb8e7a59c43253b027a67520a2b
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-24 16:11:28 +0900

    fix: Update controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, response } from "express";
+import { Request, Response } from "express";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
@@ -8,68 +8,62 @@ import { AdminRequest } from "../libs/types/member";
 const productService = new ProductService();
 
 const productController: T = {};
-/** SPA */
 
 productController.getAllProducts = async (req: Request, res: Response) => {
-      try {
-        console.log("getAllProducts");
-        const data = await productService.getAllProducts();
-        console.log("products:", data);
-    
-        res.render("products", { products: data });
-      } catch (err) {
-        console.log("Error, getAllProducts", err);
-        if (err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standard.code).json(Errors.standard);
-      }
-    };
+  try {
+    console.log("getAllProducts");
+    const data = await productService.getAllProducts();
 
-    productController.createNewProduct = async (
-      req: AdminRequest,
-      res: Response
-    ) => {
-      try {
-        console.log("createNewProduct");
-        console.log("req.files:", req.files);
-    
-        if (!req.files?.length)
-          throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+    res.render("products", { products: data });
+  } catch (err) {
+    console.log("Error, signup:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
 
- const data: ProductInput = req.body;
- data.productImages = req.files?.map((ele) => {
-  return ele.path;
-});
- 
+productController.createNewProduct = async (
+  req: AdminRequest,
+  res: Response
+) => {
+  try {
+    console.log("createNewProduct");
+    if (!req.files?.length)
+      throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
-await productService.createNewProduct(data);
+    const data: ProductInput = req.body;
+    data.productImages = req.files?.map((ele) => {
+      return ele.path.replace(/\\/g, "/");
+    });
 
-res.send(
- `<script> alert("Sucessfull creation!"); window.location.replace("admin/product/all") </script>`);
+    await productService.createNewProduct(data);
+    res.send(
+      `<script> alert("Successfully creation!"); window.location.replace("admin/product/all") </script>`
+    );
+  } catch (err) {
+    console.log("Error, createNewProduct:", err);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
 
- } catch (err) {
- console.log("Error, createNewProduct:", err);
- const message =
- err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-res.send(
- `<script> alert("${message}"); window.location.replace("admin/product/all") </script>`
-);
-}
+    res.send(
+      `<script> alert("${message}"); window.location.replace("admin/product/all") </script>`
+    );
+  }
 };
 
 productController.updateChosenProduct = async (req: Request, res: Response) => {
-      try {
-        console.log("updateChosenProduct");
-        const id = req.params.id;
-        console.log("req.params:", id);
-    
-        const result = await productService.updateChosenProduct(id, req.body);
-    
-        res.status(HttpCode.OK).json({ data: result });
-      } catch (err) {
-        console.log("Error, updateChosenProduct", err);
-        if (err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standard.code).json(Errors.standard);
-      }
-    };
-    
+  try {
+    console.log("updateChosenProduct");
+    const id = req.params.id;
+
+    const result = await productService.updateChosenProduct(id, req.body);
+
+    res.status(HttpCode.OK).json({ data: result });
+  } catch (err) {
+    console.log("Error, signup:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
 export default productController;
--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,27 +3,30 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
-import Errors, { HttpCode, Message } from "../libs/Errors";
+import { HttpCode, Message } from "../libs/Errors";
+import Errors from "../libs/Errors";
+import session from "express-session";
 
 const memberService = new MemberService();
-const restaurantController: T = {};
 
+const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.render("home"); // home.ejsga yuboradi, send| render| redirect 
+    res.render("home"); // send | render | end | redirect | json
   } catch (err) {
     console.log("Error, goHome:", err);
-    res.redirect("/admin")
+    res.redirect("/admin");
   }
 };
+
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
-    console.log("getSingup");
+    console.log("getSignup");
     res.render("signup");
   } catch (err) {
     console.log("Error, getSignup:", err);
-    res.redirect("/admin")
+    res.redirect("/admin");
   }
 };
 
@@ -33,36 +36,35 @@ restaurantController.getLogin = (req: Request, res: Response) => {
     res.render("login");
   } catch (err) {
     console.log("Error, getLogin:", err);
-    res.redirect("/admin")
+    res.redirect("/admin");
   }
 };
 
 restaurantController.processSignup = async (
-  req: AdminRequest, 
+  req: AdminRequest,
   res: Response
- ) => {
+) => {
   try {
     console.log("processSignup");
     const file = req.file;
-    if(!file) 
+    if (!file)
       throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
-
     const newMember: MemberInput = req.body;
-    newMember.memberImage = file?.path;
+    newMember.memberImage = file?.path.replace(/\\/g, "/");
     newMember.memberType = MemberType.RESTAURANT;
     const result = await memberService.processSignup(newMember);
 
     req.session.member = result;
-    req.session.save(function() {
+    req.session.save(function () {
       res.redirect("/admin/product/all");
     });
   } catch (err) {
-    console.log("Error processSignup", err);
+    console.log("Error, processSignup:", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window.location.replace('/admin/signup') </script>`);
-    
+      `<script> alert("${message}"); window.location.replace("admin/signup") </script>`
+    );
   }
 };
 
@@ -72,8 +74,6 @@ restaurantController.processLogin = async (
 ) => {
   try {
     console.log("processLogin");
-    console.log("body:", req.body);
-
     const input: LoginInput = req.body;
     const result = await memberService.processLogin(input);
 
@@ -86,57 +86,52 @@ restaurantController.processLogin = async (
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window.location.replace("/admin/login") </script>`
+      `<script> alert("${message}"); window.location.replace("admin/login") </script>`
     );
   }
 };
 
-restaurantController.logout = async (
-  req: AdminRequest, 
-  res: Response
-) => {
+restaurantController.logout = async (req: AdminRequest, res: Response) => {
   try {
     console.log("logout");
-    req.session.destroy(function() {
-  res.redirect("/admin")
+    req.session.destroy(function () {
+      res.redirect("/admin");
     });
   } catch (err) {
     console.log("Error, logout:", err);
-    res.redirect("/admin")
+    res.redirect("/admin");
   }
 };
 
-
 restaurantController.checkAuthSession = async (
-  req: AdminRequest, 
+  req: AdminRequest,
   res: Response
 ) => {
   try {
     console.log("checkAuthSession");
- if(req.session?.member) 
-  res.send(`<script> alert("${req.session.member.memberNick}" </script>`);
- else res.send(`<script> alert("${Message.NOT_AUTHENTICATED}" </script>)`);
+    if (req.session?.member)
+      res.send(`<script> alert("${req.session.member.memberNick}") </script>`);
+    else res.send(`<script> alert("${Message.NOT_AUTHENTICATED}") </script>`);
   } catch (err) {
     console.log("Error, checkAuthSession:", err);
     res.send(err);
   }
 };
 
-restaurantController.verifyRestaurant = ( 
-  req: AdminRequest, 
-  res: Response, 
+restaurantController.verifyRestaurant = (
+  req: AdminRequest,
+  res: Response,
   next: NextFunction
 ) => {
-   if (req.session?.member?.memberType === MemberType.RESTAURANT) {
-  req.member = req.session.member;
-  next();
- } else {
-  const message = Message.NOT_AUTHENTICATED;
+  if (req.session?.member?.memberType === MemberType.RESTAURANT) {
+    req.member = req.session.member;
+    next();
+  } else {
+    const message = Message.NOT_AUTHENTICATED;
     res.send(
-      `<script> alert("${message}"); window.location.replace('/admin/login'); </script>)`
-  );
- }
-}; 
-
+      `<script> alert("${message}"); window.location.replace("/admin/login"); </script>`
+    );
+  }
+};
 
 export default restaurantController;
