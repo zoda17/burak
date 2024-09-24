58b1b6534e755ca94fd05899c5216008b885c643
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-24 13:33:34 +0900

    fix: fixed Router-Admin crush

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response, response } from "express";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
-import ProductService from "../models/Product.service";  
+import ProductService from "../models/Product.service";
 import { ProductInput } from "../libs/types/product";
 import { AdminRequest } from "../libs/types/member";
 
@@ -10,64 +10,66 @@ const productService = new ProductService();
 const productController: T = {};
 /** SPA */
 
-/** SSR */
-
 productController.getAllProducts = async (req: Request, res: Response) => {
-    try{
-        console.log("getAllProducts");
-        const data = await productService.getAllProducts();
-        
-        res.render("products", {products: data });
-     }  catch (err) {
-        console.log("Error, getAllProducts:", err);
-        if(err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standard.code).json(Errors.standard);
-    }
-};
+      try {
+        console.log("getAllProducts");
+        const data = await productService.getAllProducts();
+        console.log("products:", data);
+    
+        res.render("products", { products: data });
+      } catch (err) {
+        console.log("Error, getAllProducts", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+      }
+    };
 
-productController.createNewProduct = async (
-  req: AdminRequest, 
-  res: Response
-  ) => {
-  try{
-    console.log("createNewProduct");
-    if(!req.files?.length) 
-      throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+    productController.createNewProduct = async (
+      req: AdminRequest,
+      res: Response
+    ) => {
+      try {
+        console.log("createNewProduct");
+        console.log("req.files:", req.files);
+    
+        if (!req.files?.length)
+          throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
-    const data: ProductInput = req.body;
-    data.productImages = req.files?.map((ele) => {
-      return ele.path;
-    });
+ const data: ProductInput = req.body;
+ data.productImages = req.files?.map((ele) => {
+  return ele.path;
+});
+ 
 
-    await productService.createNewProduct(data);
+await productService.createNewProduct(data);
 
-    res.send(  
-      `<script> alert("Sucessfull creation!"); window.location.replace("admin/product/all") </script>`);
+res.send(
+ `<script> alert("Sucessfull creation!"); window.location.replace("admin/product/all") </script>`);
 
-    
-  } catch (err) {
-    console.log("Error, createNewProduct:", err);
-    const message =
-    err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-    res.send(  
-      `<script> alert("${message}"); window.location.replace("admin/product/all") </script>`
-      );
-  }
+ } catch (err) {
+ console.log("Error, createNewProduct:", err);
+ const message =
+ err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+res.send(
+ `<script> alert("${message}"); window.location.replace("admin/product/all") </script>`
+);
+}
 };
 
 productController.updateChosenProduct = async (req: Request, res: Response) => {
-    try{
-      console.log("updateChosenProduct");
-      const id = req.params.id;
-      
-      const result = await productService.updateChosenProduct(id, req.body);
-
-      res.status(HttpCode.OK).json({data: result });
-    } catch (err) {
-      console.log("Error, updateChosenProduct:", err);
-      if(err instanceof Errors) res.status(err.code).json(err);
-      else res.status(Errors.standard.code).json(Errors.standard);
-    }
-};
-
+      try {
+        console.log("updateChosenProduct");
+        const id = req.params.id;
+        console.log("req.params:", id);
+    
+        const result = await productService.updateChosenProduct(id, req.body);
+    
+        res.status(HttpCode.OK).json({ data: result });
+      } catch (err) {
+        console.log("Error, updateChosenProduct", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+      }
+    };
+    
 export default productController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,41 +2,43 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
-import makeUploader from "./libs/utils/uploader";
+import makeUpLoader from "./libs/utils/uploader";
 
-/** Restaurant  */
+/** RESTAURANT */
+
 routerAdmin.get("/", restaurantController.goHome);
 routerAdmin
-    .get("/login", restaurantController.getLogin)
-    .post("/login", restaurantController.processLogin);
+  .get("/login", restaurantController.getLogin)
+  .post("/login", restaurantController.processLogin);
 routerAdmin
-.get("/signup", restaurantController.getSignup)
-.post(
-    "/signup", 
-    makeUploader("members").single("memberImage"), 
+  .get("/signup", restaurantController.getSignup)
+  .post(
+    "/signup",
+    makeUpLoader("members").single("memberImage"),
     restaurantController.processSignup
-    );
+  );
 routerAdmin.get("/logout", restaurantController.logout);
+
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
-/** Product */
+/** PRODUCT */
 routerAdmin.get(
-    "/product/all", 
-restaurantController.verifyReastaurant,
-productController.getAllProducts
+  "/product/all",
+  restaurantController.verifyRestaurant,
+  productController.getAllProducts
 );
 routerAdmin.post(
-    "/product/create", 
-    restaurantController.verifyReastaurant,
-    // uploadProductImage.single("productImage"),
-    makeUploader("products").array("productImages", 5),
-    productController.createNewProduct
+  "/product/create",
+  restaurantController.verifyRestaurant,
+  makeUpLoader("products").array("productImages", 5),
+  productController.createNewProduct
 );
 routerAdmin.post(
-    "/product/:id/", 
-    restaurantController.verifyReastaurant, 
-    productController.updateChosenProduct
+  "/product/:id",
+  restaurantController.verifyRestaurant,
+  productController.updateChosenProduct
 );
-/** User */
+
+/** USER */
 
 export default routerAdmin;
