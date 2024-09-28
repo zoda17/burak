68635e83954f3d5176d86788f9a324e33d7d397f
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-09-28 16:21:51 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,25 @@
 console.log("Signup frontend javascript file");
+
+$(function () { });
+
+function validateSignupForm() {
+const memberNick = $(".member-nick").val();
+const memberPhone = $(".member-phone").val();
+const memberPassword = $(".member-password").val();
+const confirmPassword = $(".confirm-password").val();
+
+if (
+memberNick === "" ||
+memberPhone === "" || 
+memberPassword === "" || 
+confirmPassword === "" 
+) {
+ alert("Please insert all required inputs!");
+ return false;
+}
+
+if(memberPassword !== confirmPassword) {
+ alert("Password differs, please check!");
+ return false;
+ }
+}
